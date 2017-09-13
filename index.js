1d13f4a5bcc7d056acfaeb90e218cf43a5c6b534
Alexander Zaitsev <zamazan4ik@tut.by>
2017-09-13 14:23:23 +0300

    Added moving function

--- index.js
+++ index.js
@@ -42,4 +42,53 @@ read_directory(path_directory); \n\
 };\n\
 \n\
 let text_directory = create_directory(path_directory);\n\n\
+function move_file(from, to)\n\
+{\n\
+fs.readFile('config.json', function (error, data) {\n\
+    if(error)\n\
+	{\n\
+        console.error('Cannot read copyright');\n\
+    }\n\
+    else\n\
+	{\n\
+        let copyright = JSON.parse(data.toString());\n\
+        fs.readdir(from, function(error, files)\n\
+        {\n\
+            files.forEach(function(item)\n\
+            {\n\
+                fs.stat(from + '/' + item, function(error, state)\n\
+                {\n\
+                    if(state.isDirectory())\n\
+					{\n\
+                        current_root = from + '/' + item;\n\
+                        move_file(current_root, to);\n\
+                    }\n\
+                    else\n\
+					{\n\
+                        if (path.extname(item).toLowerCase() == '.txt')\n\
+						{\n\
+                            let new_file = '';\n\
+                            fs.readFile(from + '/' + item, function(error, data)\n\
+                            {\n\
+                                if (error)\n\
+								{\n\
+                                    console.error('');\n\
+                                }\n\
+                                else\n\
+								{\n\
+                                    new_file = copyright.copyright + data.toString() + copyright.copyright;\n\
+                                    fs.writeFile(from + '/' + item, new_file, 'utf8', function () {});\n\
+                                    fs.rename(from + '/' + item, to + '/' + item, function () {});\n\
+                                }\n\
+                            });\n\
+                        }\n\
+                    }\n\
+                });\n\
+            });\n\
+        });\n\
+    }\n\
+});\n\
+}\n\
+\n\\n\
+move_file(path_directory, text_directory);\n\n\
 
