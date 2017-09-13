94299eaae1fa4fa5ee0c549485b334ec2bd726ae
Alexander Zaitsev <zamazan4ik@tut.by>
2017-09-13 14:22:16 +0300

    Added read_directory function

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const {exec} = require('child_process');
+let fs = require('fs');
+let path = require('path');
 
-const path = process.argv[2];
+let path_directory = process.argv[2];
 
-exec('touch summary.js');
-exec("echo 'const {exec} = require('child_process');\n' >> summary.js");
-exec("echo 'exec('find .')' >> summary.js");
-
-exec("mkdir temp");
-exec("find . -name '*.txt' -exec cp -t temp '{}' +");
-exec("find ./temp -name '*.txt' -type f -exec sh -c \"echo 'copyright' >> {}\" \;");
-//exec("find ./temp -name \"*.txt\" -type f -exec sh -c \"sed -i '1itask goes here' >> {}\" \;");
-
+let additional_script = "\
+let fs = require('fs');\n\
+let path = require('path');\n\
+let path_directory = '" + path_directory.replace(/\\/gi, '/') + "/' \n\
+function read_directory(root)\n\
+{\n \
+    fs.readdir(root, function(error, files)\n\
+    {\n \
+        files.forEach(function(item)\n\
+        { \n\
+            fs.stat(root + '/' + item, function(error, state)\n\
+            {\n \
+                if(state.isDirectory())\n\
+				{\n \
+                    current_root = root + '/' + item;\n \
+                    read_directory(current_root);\n \
+                }\n\
+                else\n\
+				{\n \
+                    console.log(path.relative(path_directory, root + '/' + item));\n \
+                } \n \
+            });\n \
+        });\n \
+    });}\n\n\
+read_directory(path_directory); \n\
+ 
